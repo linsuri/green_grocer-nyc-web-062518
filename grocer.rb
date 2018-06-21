aeebfbc0090ca238fe58da8cb12e763d4291395f
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-21 13:43:49 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,30 @@
 def consolidate_cart(cart)
   # code here
+  new_cart = {}
+  cart.each do |item, consolidated|
+		item_name = item.keys.first
+		if consolidated[item_name]
+			consolidated[item_name][:count] += 1
+		else
+			consolidated[item_name] = {
+				price: item[item_name][:price],
+				clearance: item[item_name][:clearance],
+				count: 1
+			}
+		end
+	end
+  
+#  new_cart = {}
+#  current_item = ""
+#  cart.each do |hash|
+#    hash.each do |item, attribute_hash|
+#      if new_cart[item] == nil
+#        new_cart[item] = item
+#      end
+#      new_cart[item] = attribute_hash
+#      new_cart[item][attribute_hash][:count] += 1
+#    end  
+#  end  
 end
 
 def apply_coupons(cart, coupons)
