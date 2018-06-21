cff389488dc5cb5dd73d7c5ae0c0642bbf9c97e7
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-21 14:43:46 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -38,6 +38,11 @@ end
 
 def apply_clearance(cart)
   # code here
+  cart.each do |item_name, attribute|
+    if attribute[:clearance] == true
+      attribute[:price] *= 0.8
+    end
+  end  
 end
 
 def checkout(cart, coupons)
