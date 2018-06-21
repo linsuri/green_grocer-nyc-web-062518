f559c953d527579e146eb9ee176f8b2fe438c269
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-21 14:50:39 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ def apply_clearance(cart)
   cart.collect do |item_name, attribute|
     if attribute[:clearance] == true
       attribute[:price] *= 0.8
-      attribute[:price].round
+      attribute[:price] = attribute[:price].round(2)
     end
   end
   cart
